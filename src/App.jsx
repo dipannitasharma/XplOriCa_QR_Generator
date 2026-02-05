ad5f1ff2a8d49d32c47f6a81f17458dea9f66859
Dipannita Sharma <dipannitasharma1@gmail.com>
2026-02-05 18:56:21 +0530

    updated size in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import DownloadButton from "./components/DownloadButton";
 export default function App() {
 
   const [text, setText] = useState("");
-  const [size, setSize] = useState(250);
+  const [size, setSize] = useState(300);
   const [format, setFormat] = useState("png");
 
   const qrRef = useRef(null);
